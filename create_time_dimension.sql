a13b9e9672c7e989ada350b3cb8395beaaf4f8b1
Asma Hsoni <123544790+Asmahosniiii@users.noreply.github.com>
2025-01-01 06:34:49 +0200

    Add files via upload

---
+++ create_time_dimension.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Time_Dimension(
+	Time_id int not null Primary key,
+	DAY varchar(50) not null,
+	MONTH varchar(50) not null,
+	QUARTER varchar(50) not null,
+	YEAR varchar(50) not null,
+
+)
+go

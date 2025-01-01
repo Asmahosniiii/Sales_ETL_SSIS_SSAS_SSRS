c2768e5884383e1521bb4a553470502b468c69e3
Asma Hsoni <123544790+Asmahosniiii@users.noreply.github.com>
2025-01-01 06:35:22 +0200

    Add files via upload

---
+++ item_dimension.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Item_Dimension(
+	Item_Key int not null Primary key,
+	Item_Name varchar(50) not null,
+	Brand varchar(50) not null,
+	Sold_By varchar(50) not null,
+	Category varchar(50) not null,
+
+
+)
+go
